2ede954211d632a0d1d36a9a2b16d0777fde6f56
Max Koon <max@koon.dev>
2022-03-19 13:57:37 -0400

    feat: add blank tiles

--- src/container/Grid.tsx
+++ src/container/Grid.tsx
@@ -1,11 +1,16 @@
-import TileBanner from "./grid/Banner";
+import TileBlank from "./grid/Blank";
 import TilePhonk from "./grid/Phonk";
 
 function Grid() {
     return (
         <>
-            <div className="grid grid-cols-1 md:gap-6 md:grid-cols-3 lg:grid-cols-6">
+            <div className="grid grid-cols-1 md:gap-6 md:grid-cols-6 lg:grid-cols-6">
                 <TilePhonk />
+                <TileBlank span={4} />
+                <TileBlank span={4} />
+                <TileBlank />
+                <TileBlank />
+                <TileBlank span={4} />
             </div>
         </>
     );
